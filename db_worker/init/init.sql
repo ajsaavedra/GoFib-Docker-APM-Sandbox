fe51e849733a6c181051d50189322c76ad08297d
Tony Saavedra <ajsaavedrax@gmail.com>
2019-11-11 22:15:01 -0800

    Updating db worker to handle delete requests

--- db_worker/init/init.sql
+++ db_worker/init/init.sql
@@ -3,5 +3,6 @@ USE gofib;
 CREATE TABLE IF NOT EXISTS sequences (
 	idx INT(3) NOT NULL,
     fib VARCHAR(24) NOT NULL,
+    elapsed VARCHAR(24) NOT NULL,
     PRIMARY KEY(idx)
 );
--- db_worker/main.go
+++ db_worker/main.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-redis/redis"
+	"encoding/json"
 	"strconv"
+	"time"
 	"fmt"
 	"os"
 )
@@ -17,28 +19,26 @@ var rdbPub *redis.Client
 
 func main() {
 	router := gin.Default()
-
-	cnxn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s",
+	cnxn := fmt.Sprintf("%s:%s@tcp(mysql)/%s",
 		os.Getenv("MYSQL_USER"),
 		os.Getenv("MYSQL_PW"),
-		os.Getenv("MYSQL_HOST"),
 		os.Getenv("MYSQL_DB"),
 	)
 	db, dberr = sql.Open("mysql", cnxn)
 	defer db.Close()
 
 	rdb, rdbPub = setRedisClient(), setRedisClient()
-	router.GET("/all", getAllValues)
-	router.Run(":3200")
-
 	go subscribe()
+	router.GET("/all", getAllValues)
+	router.DELETE("/:num", deleteFibValue)
+	router.Run(":3200")
 }
 
 func setRedisClient() *redis.Client {
 	addr := fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
 	client := redis.NewClient(&redis.Options{
 		Addr: addr,
-		Password: "",             
+		Password: "",
 	})
 
 	_, err := client.Ping().Result()
@@ -47,37 +47,37 @@ func setRedisClient() *redis.Client {
 }
 
 func subscribe() {
-	pubsub := rdb.Subscribe("memo-channel", "iter-channel")
+	pubsub := rdb.Subscribe("memo-channel")
 	_, err := pubsub.Receive()
 	handleErr(err)
 
 	for msg := range pubsub.Channel() {
-		switch msg.Channel {
-		case "memo-channel":
-			go getFibValue(msg.Payload)
-		case "iter-channel":
-		}
+		go getFibValue(msg.Payload)
 	}
 }
 
 func getFibValue(msg string) {
-	var idx, fib int
+	var idx int
+	var fib, elapsed string
 	num, _ := strconv.Atoi(msg)
 
-	err := db.QueryRow("SELECT * FROM sequences WHERE idx = ?", num).Scan(&idx, &fib)
+	err := db.QueryRow("SELECT * FROM sequences WHERE idx = ?", num).Scan(&idx, &fib, &elapsed)
 
 	if err != nil {
-		insertFibValue(num)
+		if err == sql.ErrNoRows {
+			insertFibValue(num)
+		}
 	}
 }
 
 func getAllValues(c *gin.Context) {
 	type Fib struct {
-		Idx int	 `json:"idx"`
-		Fib string `json:"fib"`
+		Idx int			`json:"idx"`
+		Fib string		`json:"fib"`
+		Elapsed string	`json:"elapsed"`
 	}
 
-	rows, err := db.Query("SELECT idx, fib FROM sequences")
+	rows, err := db.Query("SELECT idx, fib, elapsed FROM sequences")
 	defer rows.Close()
 	
 	if err != nil {
@@ -88,10 +88,10 @@ func getAllValues(c *gin.Context) {
 
 	for rows.Next() {
 		var idx int
-		var fib string
-		rows.Scan(&idx, &fib)
+		var fib, elapsed string
+		rows.Scan(&idx, &fib, &elapsed)
 
-		values = append(values, Fib{idx, fib})
+		values = append(values, Fib{idx, fib, elapsed})
 	}
 
 	c.JSON(200, gin.H{
@@ -100,14 +100,31 @@ func getAllValues(c *gin.Context) {
 }
 
 func insertFibValue(idx int) {
+	start := time.Now()
 	fib := memoFib(idx, map[int]int{ 0:0, 1:1 })
-	stmt, _ := db.Prepare("INSERT INTO sequences(idx, fib) VALUES (?, ?)")
-	stmt.Exec(idx, fib)
-	emitFib(fib)
+	elapsed := time.Since(start).String()
+	stmt, _ := db.Prepare("INSERT INTO sequences(idx, fib, elapsed) VALUES (?, ?, ?)")
+	stmt.Exec(idx, fib, elapsed)
+	emitFib(idx, fib, elapsed)
 }
 
-func emitFib(fib int) {
-	err := rdbPub.Publish("emit-channel", fib).Err()
+func deleteFibValue(c *gin.Context) {
+	stmt, _ := db.Prepare("DELETE FROM sequences WHERE idx = ?")
+	stmt.Exec(c.Param("num"))
+	c.JSON(200, gin.H{
+		"payload": c.Param("num"),
+	})
+}
+
+func emitFib(idx int, fib int, elapsed string) {
+	type data struct {
+		Idx int
+		Fib int
+		Elapsed string
+	}
+	payload := &data{idx, fib, elapsed}
+	message, _ := json.Marshal(payload)
+	err := rdbPub.Publish("emit-channel", message).Err()
 	handleErr(err)
 }
 
